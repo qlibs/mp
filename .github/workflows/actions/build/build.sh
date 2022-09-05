e8645f14c0e86ca21c623314020a6e6f6be8c0b4
Kris Jusiak <kris@jusiak.net>
2022-09-05 21:54:28 +0000

    :books: [README] g++-12

--- .github/workflows/actions/build/build.sh
+++ .github/workflows/actions/build/build.sh
@@ -3,8 +3,8 @@
 cd /github/home/mp
 git submodule update --init
 
-mkdir build
-cd build
+mkdir build-$1
+cd build-$1
 CXX=$1 cmake .. -DBOOST_MP_BUILD_TESTS=ON -DBOOST_MP_BUILD_EXAMPLES=ON
 make -j8
 ctest --output-on-failure
