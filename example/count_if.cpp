937517760cd5c25997d57c864bebceda673eb255
Kris Jusiak <kris@jusiak.net>
2022-09-08 22:31:00 +0000

    :new: [example] Support for ranges returning values - for example count_if

---
+++ example/count_if.cpp
@@ -0,0 +1,27 @@
+//
+// Copyright (c) 2022 Kris Jusiak (kris at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <boost/mp.hpp>
+#if defined(__clang__)
+#include <ranges>
+
+namespace mp = boost::mp;
+
+using boost::mp::operator<<;
+using boost::mp::operator|;
+
+// clang-format off
+template <auto List>
+constexpr auto count_if = List
+  | std::ranges::count_if<<([]<class T> { return std::is_pointer_v<T>; });
+
+static_assert(count_if<mp::list<int*, double>()> == 1);
+// clang-format on
+
+#endif
+
+int main() {}
