5e3849dfba5893744effac8a1900c91e475afcc7
Kris Jusiak <kris@jusiak.net>
2022-09-21 15:44:09 +0000

    :art: [example] state machine with objects

--- example/state_machine.cpp
+++ example/state_machine.cpp
@@ -14,6 +14,10 @@
 namespace mp = boost::mp;
 
 // clang-format off
+template<class... Ts> struct pool : Ts...{
+  constexpr explicit(true) pool(Ts... ts) : Ts{std::move(ts)}... {}
+};
+
 constexpr auto inherit = []<class... Ts> {
   struct : Ts... { } _;
   return _;
@@ -46,17 +50,20 @@ constexpr auto by_name = []<mp::fixed_string... Names> {
 };
 
 namespace back {
-template<class TransitionTable>
-class sm {
-  static constexpr auto transitions = TransitionTable{};
+template<class> class sm;
+template<template<class...> class TList, class... Transitions>
+class sm<TList<Transitions...>> {
+  static constexpr auto transitions = mp::list<Transitions...>();
   static constexpr auto events = transitions
     | std::views::transform([]<class T>() -> typename T::event {})
     | unique<by_type>
     ;
+
   static constexpr auto states = transitions
     | []<class... Ts>() { return mp::value_list<Ts::src..., Ts::dst...>(); }
     | unique_v<by_name>
     ;
+
   template<mp::fixed_string State>
   static constexpr auto state_id = states
     | []<mp::fixed_string... States> {
@@ -64,23 +71,28 @@ class sm {
       return std::distance(std::begin(states), std::ranges::find(states, State));
     }
     ;
+
   static constexpr auto transition = []<class... Ts> {
-    return [](const auto& event, auto& current_state) {
+    return [](const auto& event, auto& current_state, auto& transitions) {
       return (... or [&] {
-        if (Ts{}.execute(event)) {
+        if (static_cast<Ts&>(transitions).execute(event)) {
           current_state = state_id<Ts::dst>;
           return true;
         }
         return false;
       }());
+      return false;
     };
   };
+
   static constexpr auto mappings = events
     | std::views::transform([]<class TEvent> {
-        return std::pair{TEvent{}, states
+        return std::pair{std::type_identity<TEvent>{}, states
           | std::views::transform([]<mp::fixed_string State> {
               return transitions
-                | std::views::filter([]<class T> { return T::src == State and std::same_as<typename T::event, TEvent>; })
+                | std::views::filter([]<class T> {
+                    return std::string_view{T::src} == State and
+                           std::same_as<typename T::event, TEvent>; })
                 | transition;
             })
         };
@@ -88,14 +100,14 @@ class sm {
     ;
 
  public:
-  constexpr explicit(false) sm(TransitionTable) {}
+  constexpr explicit(false) sm(const TList<Transitions...>& transition_table) : transition_table_{transition_table} {}
 
   template<class TEvent> constexpr auto process_event(const TEvent& event) -> void {
-    [this, &event]<class T>(const std::pair<TEvent, T>& transitions) {
+    [this, &event]<class T>(const std::pair<std::type_identity<TEvent>, T>& transitions) {
       auto& [_, ts] = transitions;
       mp::for_each([&](auto index, auto transition) {
         if (index == current_state_) {
-          transition(event, current_state_);
+          transition(event, current_state_, transition_table_);
           return true;
         }
         return false;
@@ -110,6 +122,7 @@ class sm {
 
  private:
   std::size_t current_state_{};
+  [[no_unique_address]] TList<Transitions...> transition_table_{};
 };
 } // namespace back
 
@@ -121,22 +134,26 @@ struct transition {
   static constexpr auto dst = Dst;
   using event = TEvent;
 
-  TGuard guard;
-  TAction action;
+  [[no_unique_address]] TGuard guard;
+  [[no_unique_address]] TAction action;
 
   template<class T>
-  [[nodiscard]] constexpr auto operator+(T) const {
-    return transition<Src, typename T::event, decltype(T::guard), decltype(T::action)>{};
+  [[nodiscard]] constexpr auto operator+(const T& t) const {
+    return transition<Src, typename T::event, decltype(T::guard), decltype(T::action)>{.guard = t.guard, .action = t.action};
   }
 
   template<class T>
-  [[nodiscard]] constexpr auto operator/(T) const {
-    return transition<Src, TEvent, TGuard, T>{};
+  [[nodiscard]] constexpr auto operator/(const T& action) const {
+    return transition<Src, TEvent, TGuard, T>{.guard = guard, .action = action};
   }
 
   template<class T>
-  [[nodiscard]] constexpr auto operator=(T) const {
-    return transition<src, TEvent, TGuard, TAction, T::src>{};
+  [[nodiscard]] constexpr auto operator=(const T&) const {
+    return transition<src, TEvent, TGuard, TAction, T::src>{.guard = guard, .action = action};
+  }
+
+  [[nodiscard]] constexpr auto operator*() const {
+    return *this;
   }
 
   [[nodiscard]] constexpr auto execute(const auto& event) -> bool {
@@ -151,35 +168,45 @@ struct transition {
 namespace detail {
 template<class T> struct event {
   template<class TGuard>
-  [[nodiscard]] constexpr auto operator[](TGuard) const {
-    return transition<"", T, TGuard>{};
+  [[nodiscard]] constexpr auto operator[](const TGuard& guard) const {
+    return transition<"", T, TGuard>{.guard = guard, .action = none{}};
   }
 };
-}
+} // namespace detail
 
 template<class T> constexpr auto event = detail::event<T>{};
 
 template <mp::fixed_string Str> constexpr auto operator""_s() { return transition<Str>{}; }
 } // namespace front
 
-template<class... Ts> [[nodiscard]] constexpr auto transition_table(Ts...) -> mp::type_list<Ts...> { return {}; };
-using back::sm;
+template<class... Ts> struct transition_table : pool<Ts...> {
+  constexpr explicit(false) transition_table(Ts... ts) : pool<Ts...>{std::move(ts)...} {}
+};
+
 using front::event;
 using front::operator""_s;
+
+template<class Fn>
+struct sm : back::sm<decltype(std::declval<Fn>()())> {
+  constexpr explicit(false) sm(Fn fn) : back::sm<decltype(std::declval<Fn>()())>{fn()} {}
+};
 
 #include <cassert>
 
 int main() {
   struct e {};
-  auto guard  = []([[maybe_unused]] const auto& event) { std::puts("guard"); return true; };
-  auto action = []([[maybe_unused]] const auto& event) { std::puts("action"); };
 
-  sm sm{
-    transition_table(
-      "s1"_s + event<e> [ guard ] / action = "s2"_s,
-      "s2"_s + event<e> [ guard ] / action = "s1"_s
-    )
+  auto transitions = [] {
+    auto guard  = []([[maybe_unused]] const auto& event) { std::puts("guard"); return true; };
+    auto action = []([[maybe_unused]] const auto& event) { std::puts("action"); };
+
+    return transition_table{
+     *"s1"_s + event<e> [ guard ] / action = "s2"_s,
+      "s2"_s + event<e> [ guard ] / action = "s1"_s,
+    };
   };
+
+  sm sm{transitions};
 
   assert(sm.is<"s1">());
   sm.process_event(e{});
